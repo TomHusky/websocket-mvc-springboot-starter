347d5b4251aadea5f818bffd6bd68859b71629f2
luowj <1677900582@qq.com>
2021-09-13 09:36:24 +0800

    加入登录验证拦截机制

--- src/main/java/io/github/tomhusky/websocket/configuration/WebSocketMvcAutoConfiguration.java
+++ src/main/java/io/github/tomhusky/websocket/configuration/WebSocketMvcAutoConfiguration.java
@@ -1,17 +1,12 @@
 package io.github.tomhusky.websocket.configuration;
 
-import cn.hutool.core.util.StrUtil;
 import io.github.tomhusky.websocket.SocketMsgHandler;
-import io.github.tomhusky.websocket.interceptor.LoginValidIntercept;
 import io.github.tomhusky.websocket.interceptor.SocketInterceptor;
-import io.github.tomhusky.websocket.listener.LoginServlet;
 import io.github.tomhusky.websocket.listener.WebSocketMvcStart;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,18 +44,5 @@ public class WebSocketMvcAutoConfiguration {
     @Bean
     public SocketMsgHandler socketMsgHandler() {
         return new SocketMsgHandler();
-    }
-
-    @ConditionalOnBean(LoginValidIntercept.class)
-    @Bean
-    public ServletRegistrationBean<LoginServlet> getServletRegistrationBean() {
-        ServletRegistrationBean<LoginServlet> bean = new ServletRegistrationBean<>(new LoginServlet());
-        //访问路径
-        if (StrUtil.isNotBlank(properties.getLoginPath())) {
-            bean.addUrlMappings(properties.getLoginPath());
-        } else {
-            bean.addUrlMappings("/websocket/login");
-        }
-        return bean;
     }
 }
